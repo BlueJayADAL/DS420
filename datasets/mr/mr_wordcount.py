b54fea58ea01cd1865fef2da95202dd0f3c2daf0
Peilong <long880420@gmail.com>
2020-09-01 21:05:58 -0400

    2nd version wordcount

--- datasets/mr/mr_wordcount.py
+++ datasets/mr/mr_wordcount.py
@@ -1,8 +1,7 @@
 from mrjob.job import MRJob
-
+from mrjob.step import MRStep
 
 class MRWordCounter(MRJob):
-
     def mapper(self, _, line):
         for word in line.split():
             yield word, 1
@@ -10,6 +9,11 @@ class MRWordCounter(MRJob):
     def reducer(self, word, occurrences):
         yield word, sum(occurrences)
 
+    def steps(self):
+        return [
+            MRStep(mapper = self.mapper,
+            reducer = self.reducer)
+    ]
 
 if __name__ == '__main__':
     MRWordCounter.run()
