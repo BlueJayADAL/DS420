2b0ed80a5fe929dbfc42e9141848206803da182b
Peilong <long880420@gmail.com>
2020-09-01 21:40:22 -0400

    rename functions

--- datasets/mr/mr_wordcount.py
+++ datasets/mr/mr_wordcount.py
@@ -2,17 +2,17 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 class MRWordCounter(MRJob):
-    def mapper(self, _, line):
+    def mapper_split_on_word(self, _, line):
         for word in line.split():
             yield word, 1
 
-    def reducer(self, word, occurrences):
+    def reducer_count_word(self, word, occurrences):
         yield word, sum(occurrences)
 
     def steps(self):
         return [
-            MRStep(mapper = self.mapper,
-            reducer = self.reducer)
+            MRStep(mapper = self.mapper_split_on_word,
+            reducer = self.reducer_count_word)
     ]
 
 if __name__ == '__main__':
